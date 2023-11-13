9d1f2bc8f4c8a9015e2b53aa0f2e092bd58bd310
Ethan <ebrogames@gmail.com>
2023-11-13 00:17:40 -0500

    push code

---
+++ Patches/SubmitChat.cs
@@ -0,0 +1,82 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+namespace QuickRestart.Patches
+{
+    [HarmonyPatch(typeof(HUDManager), "SubmitChat_performed")]
+    public class SubmitChat
+    {
+        // TODO: make this not ugly
+        private static bool Prefix(HUDManager __instance, ref InputAction.CallbackContext context)
+        {
+            if (!context.performed)
+            {
+                return true;
+            }
+            if (string.IsNullOrEmpty(__instance.chatTextField.text))
+            {
+                return true;
+            }
+            PlayerControllerB local = GameNetworkManager.Instance.localPlayerController;
+            if (local == null)
+            {
+                return true;
+            }
+            StartOfRound manager = local.playersManager;
+            if (manager == null)
+            {
+                return true;
+            }
+            string text = __instance.chatTextField.text;
+            if (Plugin.verifying)
+            {
+                if (text == "CONFIRM")
+                {
+                    ResetTextbox(__instance, local);
+                    if (!local.isInHangarShipRoom || !manager.inShipPhase || manager.travellingToNewLevel)
+                    {
+                        Plugin.SendChatMessage("Cannot restart, ship must be in orbit.");
+                        return false;
+                    }
+                    Plugin.AcceptRestart(manager);
+                    return false;
+                }
+                if (text == "DENY")
+                {
+                    ResetTextbox(__instance, local);
+                    Plugin.DeclineRestart();
+                    return false;
+                }
+                return true;
+            }
+            if (text == "/restart")
+            {
+                ResetTextbox(__instance, local);
+                if (!GameNetworkManager.Instance.isHostingGame)
+                {
+                    Plugin.SendChatMessage("Only the host can restart.");
+                    return false;
+                }
+                if (!local.isInHangarShipRoom || !manager.inShipPhase || manager.travellingToNewLevel)
+                {
+                    Plugin.SendChatMessage("Cannot restart, ship must be in orbit.");
+                    return false;
+                }
+                Plugin.ConfirmRestart();
+                return false;
+            }
+            return true;
+        }
+
+        private static void ResetTextbox(HUDManager manager, PlayerControllerB local)
+        {
+            local.isTypingChat = false;
+            manager.chatTextField.text = "";
+            EventSystem.current.SetSelectedGameObject(null);
+            manager.PingHUDElement(manager.Chat, 2f, 1f, 0.2f);
+            manager.typingIndicator.enabled = false;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace QuickRestart;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static bool verifying = false;
+
+    private Harmony harmony;
+    private static MethodInfo chat;
+
+    private void Awake()
+    {
+        harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+
+        chat = AccessTools.Method(typeof(HUDManager), "AddChatMessage");
+
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} loaded!");
+    }
+
+    public static void SendChatMessage(string message)
+    {
+        chat?.Invoke(HUDManager.Instance, new object[] { message, "" });
+        HUDManager.Instance.lastChatMessage = "";
+    }
+
+    public static void ConfirmRestart()
+    {
+        verifying = true;
+        SendChatMessage("Are you sure? Type CONFIRM or DENY.");
+    }
+
+    public static void AcceptRestart(StartOfRound manager)
+    {
+        SendChatMessage("Restart confirmed.");
+        verifying = false;
+
+        int[] stats = new int[]
+        {
+                manager.gameStats.daysSpent,
+                manager.gameStats.scrapValueCollected,
+                manager.gameStats.deaths,
+                manager.gameStats.allStepsTaken
+        };
+        manager.FirePlayersAfterDeadlineClientRpc(stats);
+    }
+
+    public static void DeclineRestart()
+    {
+        SendChatMessage("Restart aborted.");
+        verifying = false;
+    }
+}
